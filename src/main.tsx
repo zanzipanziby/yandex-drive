9ee0a2128fd19a1704bd651ca0448964cfb8fb09
ZanziPanziBY <zanzipanziby@gmail.com>
2023-09-29 08:41:05 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,5 @@ import "@fontsource/roboto/700.css";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 );
